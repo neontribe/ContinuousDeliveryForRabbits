4a5e89a111ad53d6c582dfb3ca96e005ef397a7a
Rupert Redington <rupert@neontribe.co.uk>
2016-04-18 21:58:46 +0100

    Stubbed all up

--- test/rabbit_test.js
+++ test/rabbit_test.js
@@ -1,4 +1,5 @@
 var expect = require('chai').expect;
+var sinon = require('sinon');
 var Rabbit = require('../app/rabbit.js');
 
 
@@ -35,12 +36,63 @@ describe('A Rabbit', function(){
       expect(rabbit).to.respondTo('play');
     });
 
-    it('which loads a sound', function(){
-
+    it('which passes a url from "sounds" to the "_player"', function(){
+      var stubbed_player = sinon.stub(rabbit, '_player');
+      rabbit.play();
+      expect(stubbed_player.calledOnce).to.equal(true);
+      expect(stubbed_player.getCall(0).args[0]).to.be.oneOf(rabbit.sounds);
     });
+
+    it('which causes an XMLHttpRequest to happen', function(){
+      // Mock the XMLHttpRequest using sinon's helper
+      var requests = [];
+      var xhr = global.XMLHttpRequest = sinon.useFakeXMLHttpRequest();
+      xhr.onCreate = function(req){
+        requests.push(req);
+      }
+      var ac = global.AudioContext = FakeAudioContext;
+      rabbit.play();
+      expect(requests).to.have.length(1);
+    });
+
+    it('which supplies data and commands to the AudioContext', function(){
+      var requests = [];
+      var xhr = global.XMLHttpRequest = sinon.useFakeXMLHttpRequest();
+      xhr.onCreate = function(req){
+        requests.push(req);
+      }
+
+      // Mock the AudioContext
+      var ac = global.AudioContext = FakeAudioContext;
+      var dAd = sinon.spy(global.AudioContext.prototype, 'decodeAudioData');
+      var cBs = sinon.spy(global.AudioContext.prototype, 'createBufferSource');
+      var sourceConnect = sinon.spy(global.AudioContext.prototype, 'connect');
+      var sourceStart = sinon.spy(global.AudioContext.prototype, 'start');
+
+      rabbit.play();
+      requests[0].respond(200);
+      expect(dAd.calledOnce).to.equal(true);
+      expect(cBs.calledOnce).to.equal(true);
+      expect(sourceConnect.calledWith('speakers')).to.equal(true);
+      expect(sourceStart.calledWith(0)).to.equal(true);
+
+    })
 
   });
 
-
+  function FakeAudioContext () {
+    this.destination = 'speakers';
+  }
+  FakeAudioContext.prototype.decodeAudioData = function(response, callback) {
+    callback();
+  };
+  FakeAudioContext.prototype.createBufferSource = function(response, callback) {
+    return {
+      connect: this.connect,
+      start: this.start
+    };
+  };
+  FakeAudioContext.prototype.connect = function() {};
+  FakeAudioContext.prototype.start = function(response, callback) {};
 
 });
