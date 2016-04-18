e3974c1f3820484ddbe1c1ce82f91f376d95f0a0
Rupert Redington <rupert@neontribe.co.uk>
2016-04-18 19:23:35 +0100

    Extend tests to chooseImage

--- test/rabbit_test.js
+++ test/rabbit_test.js
@@ -3,6 +3,11 @@ var Rabbit = require('../app/rabbit.js');
 
 
 describe('A Rabbit', function(){
+  var rabbit;
+
+  beforeEach('Make a Rabbit for testing', function(){
+    rabbit = new Rabbit();
+  });
 
   it('should be productive when invoked as a constructor', function(){
     var rabbit = new Rabbit();
@@ -10,15 +15,19 @@ describe('A Rabbit', function(){
   });
 
   it('should have a "pics" property containing an array', function(){
-    var rabbit = new Rabbit();
     expect(rabbit.pics).to.be.an.instanceOf(Array);
     expect(rabbit.pics).to.have.length.above(0);
   });
 
   it('should have a "sounds" property containing an array', function(){
-    var rabbit = new Rabbit();
     expect(rabbit.sounds).to.be.an.instanceOf(Array);
     expect(rabbit.sounds).to.have.length.above(0);
   });
+
+  it('should have a "chooseImage" method which returns a url from "pics"', function(){
+    expect(rabbit).to.respondTo('chooseImage');
+    expect(rabbit.pics).to.include(rabbit.chooseImage());
+  });
+
 
 });
