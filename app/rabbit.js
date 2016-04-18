d78dc5f87b6db850eddba89bd17272649948ceb8
Rupert Redington <rupert@neontribe.co.uk>
2016-04-18 19:19:21 +0100

    Protoype Rabbit

--- app/rabbit.js
+++ app/rabbit.js
@@ -1,33 +1,38 @@
-(function(window, undefined){
 
-  function loadAndPlay(url) {
-    var request = new XMLHttpRequest();
-    var context = new window.AudioContext();
-    request.open('GET', url, true);
-    request.responseType = 'arraybuffer';
-    request.onload = function() {
-      context.decodeAudioData(request.response, function(buffer) {
-        var source = context.createBufferSource();
-        source.buffer = buffer;
-        source.connect(context.destination);
-        source.start(0);
-      });
-    }
-    request.send();
+// Wrap fetching the audio file and playing it
+function loadAndPlay(url) {
+  var request = new XMLHttpRequest();
+  var context = new window.AudioContext();
+  request.open('GET', url, true);
+  request.responseType = 'arraybuffer';
+  request.onload = function() {
+    context.decodeAudioData(request.response, function(buffer) {
+      var source = context.createBufferSource();
+      source.buffer = buffer;
+      source.connect(context.destination);
+      source.start(0);
+    });
   }
+  request.send();
+}
 
-  window.rabbit = {
-    pics: ['assets/r1.jpg', 'assets/r2.jpg', 'assets/r3.jpg'],
-    sounds: ['assets/sfx1.mp3'],
-    pick: function(src) {
-      return src[Math.floor(Math.random() * src.length)];
-    },
-    chooseImage: function() {
-      return this.pick(this.pics);
-    },
-    play: function () {
-      loadAndPlay(this.pick(this.sounds));
-    }
-  };
+function pick(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
 
-})(window);
+function Rabbit () {
+  this.pics = ['assets/r1.jpg', 'assets/r2.jpg', 'assets/r3.jpg'];
+  this.sounds = ['assets/sfx1.mp3'];
+};
+
+Rabbit.prototype.chooseImage = function() {
+  return pick(this.pics);
+};
+Rabbit.prototype.play = function () {
+  loadAndPlay(pick(this.sounds));
+};
+
+// Export node module.
+if (typeof module !== 'undefined' && module.hasOwnProperty('exports')) {
+    module.exports = Rabbit;
+}
