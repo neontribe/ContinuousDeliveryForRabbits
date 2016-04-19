58ee65fade203da19e63b3276737046c07ec56b7
Rupert Redington <rupert@neontribe.co.uk>
2016-04-19 09:58:51 +0100

    Sauce for Travis

--- test/nightwatch.js
+++ test/nightwatch.js
@@ -1,52 +1,41 @@
+const TRAVIS_JOB_NUMBER = process.env.TRAVIS_JOB_NUMBER;
 module.exports = {
-  "src_folders" : ['test/e2e/'],
-  "output_folder" : "reports",
-  "custom_commands_path" : "",
-  "custom_assertions_path" : "",
-  "page_objects_path" : "",
-  "globals_path" : "",
+  src_folders: ['test/e2e/'],
+  output_folder: 'reports',
+  custom_commands_path: '',
+  custom_assertions_path: '',
+  page_objects_path: '',
+  globals_path: '',
 
-  "selenium" : {
-    "start_process" : false,
-    "server_path" : "",
-    "log_path" : "",
-    "host" : "127.0.0.1",
-    "port" : 4444,
-    "cli_args" : {
-      "webdriver.chrome.driver" : "",
-      "webdriver.ie.driver" : ""
+  selenium: {
+    start_process: false,
+    server_path: '',
+    log_path: '',
+    host:'127.0.0.1',
+    port: 4444,
+    cli_args: {
+      'webdriver.chrome.driver': '',
+      'webdriver.ie.driver': ''
     }
   },
 
-  "test_settings" : {
-    "default" : {
-      "launch_url" : "http://ondemand.saucelabs.com:80",
-      "selenium_port"  : 80,
-      "selenium_host"  : "ondemand.saucelabs.com",
-      "silent": true,
-      "username": process.env.SAUCE_USERNAME,
-      "access_key": process.env.SAUCE_ACCESS_KEY,
-      "screenshots" : {
-        "enabled" : false,
-        "path" : ""
-      }
-    },
-    "local" : {
-      "launch_url" : "http://localhost",
-      "selenium_port"  : 4444,
-      "selenium_host"  : "localhost",
-      "silent": true,
-      "screenshots" : {
-        "enabled" : false,
-        "path" : ""
+  test_settings: {
+    default: {
+      launch_url: 'http://ondemand.saucelabs.com:80',
+      selenium_port: 80,
+      selenium_host: 'ondemand.saucelabs.com',
+      silent: true,
+      username: process.env.SAUCE_USERNAME,
+      access_key: process.env.SAUCE_ACCESS_KEY,
+      screenshots: {
+        enabled: false,
+        path: ''
       },
-      "desiredCapabilities": {
-        "browserName": "firefox",
-        "javascriptEnabled": true,
-        "acceptSslCerts": true
+      desiredCapabilities: {
+        build: build-${TRAVIS_JOB_NUMBER},
+        'tunnel-identifier': TRAVIS_JOB_NUMBER,
       }
     },
-
     chrome: {
       desiredCapabilities: {
         browserName: 'chrome',
@@ -54,7 +43,6 @@ module.exports = {
         version: '47'
       }
     },
-
     ie11: {
       desiredCapabilities: {
         browserName: 'internet explorer',
